5b94056efed20c61db8ebfddcdad1e383b39129b
labidi-mahdi <labidimahdi99@gmail.com>
2024-10-09 03:38:21 +0100

    first commit

---
+++ src/app/about/page.jsx
@@ -0,0 +1,22 @@
+export default function About() {
+  return (
+    <div className='max-w-6xl mx-auto p-3 space-y-4'>
+      <h1 className='text-2xl font-medium text-amber-600'>About</h1>
+      <p>
+      Welcome to my movie database web app! This project was created out of my passion for movies and software development, combining both to provide a comprehensive platform for movie enthusiasts. The app is designed to offer a rich database of films from around the globe, along with the latest news, reviews, and trailers.
+      </p>
+
+      <p>
+      The database is regularly updated with new releases, giving users access to the freshest content in the world of cinema. You can search for movies by title, director, actor, genre, or release date, making it easy to discover the perfect film for any occasion.
+      </p>
+
+      <p>
+      Beyond the database, I’ve added a community section where users can share their opinions on recent releases and engage in discussions with fellow movie lovers. There’s also a dedicated area for news and trailers, so you never miss an update from the world of film.
+      </p>
+
+      <p>
+      Thank you for checking out this project! If you have any feedback or ideas for improvement, feel free to reach out—I’m always looking for ways to enhance the experience. Happy browsing!
+      </p>
+    </div>
+  );
+}
